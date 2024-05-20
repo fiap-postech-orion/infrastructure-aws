c216b46790953f5de04dc5a2d9f044f0a022b9e6
Pietro <victor_pietro@hotmail.com>
2024-05-20 16:48:46 -0300

    Adicionando outros ECRs para a arquitetura

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,41 @@
-resource "aws_ecr_repository" "ecr-techchallenge" {
-  name         = "techchallenge-eks"
+resource "aws_ecr_repository" "cliente-api" {
+  name         = "cliente-api-repository"
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "pagamento-api" {
+  name         = "pagamento-api-repository"
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "pedido-api" {
+  name         = "pedido-api-repository"
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "producao-api" {
+  name         = "producao-api-repository"
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "produto-api" {
+  name         = "produto-api-repository"
   force_delete = true
 
   image_scanning_configuration {
