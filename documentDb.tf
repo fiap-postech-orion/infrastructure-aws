56f7e5a9dc0556bddf44fba3e9d7b1d3c665d7fe
Pietro <victor_pietro@hotmail.com>
2024-05-20 22:07:22 -0300

    Changing vpc for documentdb

--- documentDb.tf
+++ documentDb.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "docdb_sg" {
   name        = "docdb-public-access-sg"
   description = "Allows access to DocumentDB from anywhere"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 27017
@@ -27,7 +27,7 @@ resource "aws_docdb_cluster" "mongo_cluster" {
   engine                 = "docdb"
   master_username        = "foo"
   master_password        = "mustbeeightchars"
-  db_subnet_group_name   = aws_db_subnet_group.rds_public_subnet_group.name
+  db_subnet_group_name   = aws_db_subnet_group.docdb_subnet_group.name
   vpc_security_group_ids = [aws_security_group.docdb_sg.id]
 }
 
@@ -36,4 +36,36 @@ resource "aws_docdb_cluster_instance" "mongo_instance" {
   identifier         = "my-docdb-cluster-instance-${count.index}"
   cluster_identifier = aws_docdb_cluster.mongo_cluster.id
   instance_class     = "db.r5.large"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_db_subnet_group" "docdb_subnet_group" {
+  name       = "my-docdb-subnet-group"
+  subnet_ids = [aws_subnet.public.id]
 }
