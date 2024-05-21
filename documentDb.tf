7e2810e843b9a0ff449df3cb3bddea31c3a04756
Pietro <victor_pietro@hotmail.com>
2024-05-20 21:33:20 -0300

    Adicionando egress no arquivo

--- documentDb.tf
+++ documentDb.tf
@@ -24,4 +24,11 @@ resource "aws_security_group" "docdb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
